415d8d656a5329255b114e2872162aa5ab663a6e
Ethan Lee <flibitijibibo@gmail.com>
2018-05-14 12:20:55 -0400

    ref instead of out

--- csharp/FAudio.cs
+++ csharp/FAudio.cs
@@ -1835,7 +1835,7 @@ public static class FAudio
 		ref F3DAUDIO_LISTENER pListener,
 		ref F3DAUDIO_EMITTER pEmitter,
 		uint Flags,
-		out F3DAUDIO_DSP_SETTINGS pDSPSettings
+		ref F3DAUDIO_DSP_SETTINGS pDSPSettings
 	);
 
 	#endregion
@@ -1916,7 +1916,7 @@ public static class FAudio
 		byte[] F3DInstance, // F3DAUDIO_HANDLE
 		ref F3DAUDIO_LISTENER pListener,
 		ref F3DAUDIO_EMITTER pEmitter,
-		out F3DAUDIO_DSP_SETTINGS pDSPSettings
+		ref F3DAUDIO_DSP_SETTINGS pDSPSettings
 	);
 
 	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
