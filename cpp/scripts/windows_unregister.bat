7dd2af955936f1ff0bc306df7946be4757c52e01
Ethan Lee <flibitijibibo@gmail.com>
2018-08-23 16:41:01 -0400

    Add XACTEngine to register scripts

--- cpp/scripts/windows_unregister.bat
+++ cpp/scripts/windows_unregister.bat
@@ -8,6 +8,14 @@ CALL :register XAudio2_4.dll
 CALL :register XAudio2_5.dll
 CALL :register XAudio2_6.dll
 CALL :register XAudio2_7.dll
+CALL :register XACTEngine3_0.dll
+CALL :register XACTEngine3_1.dll
+CALL :register XACTEngine3_2.dll
+CALL :register XACTEngine3_3.dll
+CALL :register XACTEngine3_4.dll
+CALL :register XACTEngine3_5.dll
+CALL :register XACTEngine3_6.dll
+CALL :register XACTEngine3_7.dll
 
 EXIT /B %ERRORLEVEL%
 
