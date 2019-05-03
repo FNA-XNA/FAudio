2fc10daa2a8894f04003620bee673f83a5697c99
Ethan Lee <flibitijibibo@gmail.com>
2019-05-03 14:43:31 -0400

    A wee bit more tabbing...

--- src/FAudio_internal.c
+++ src/FAudio_internal.c
@@ -1133,8 +1133,8 @@ static void FAUDIOCALL FAudio_INTERNAL_GenerateOutput(FAudio *audio, float *outp
 		return;
 	}
 
-    FAudio_PlatformLockMutex(audio->operationLock);
-    LOG_MUTEX_LOCK(audio, audio->operationLock)
+	FAudio_PlatformLockMutex(audio->operationLock);
+	LOG_MUTEX_LOCK(audio, audio->operationLock)
 
 	/* ProcessingPassStart callbacks */
 	FAudio_PlatformLockMutex(audio->callbackLock);
@@ -1266,8 +1266,8 @@ static void FAUDIOCALL FAudio_INTERNAL_GenerateOutput(FAudio *audio, float *outp
 	FAudio_PlatformUnlockMutex(audio->callbackLock);
 	LOG_MUTEX_UNLOCK(audio, audio->callbackLock)
 
-    FAudio_PlatformUnlockMutex(audio->operationLock);
-    LOG_MUTEX_UNLOCK(audio, audio->operationLock)
+	FAudio_PlatformUnlockMutex(audio->operationLock);
+	LOG_MUTEX_UNLOCK(audio, audio->operationLock)
 
 	LOG_FUNC_EXIT(audio)
 }
