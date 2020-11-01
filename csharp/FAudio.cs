5021236332df45833dd63f532d9894454f0221d8
Ethan Lee <flibitijibibo@gmail.com>
2020-11-01 09:42:17 -0500

    20.11

--- csharp/FAudio.cs
+++ csharp/FAudio.cs
@@ -79,7 +79,7 @@ public static class FAudio
 
 	public const uint FAUDIO_ABI_VERSION =		 0;
 	public const uint FAUDIO_MAJOR_VERSION =	20;
-	public const uint FAUDIO_MINOR_VERSION =	10;
+	public const uint FAUDIO_MINOR_VERSION =	11;
 	public const uint FAUDIO_PATCH_VERSION =	 0;
 
 	public const uint FAUDIO_COMPILED_VERSION = (
