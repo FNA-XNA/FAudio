cfe7f27fe5470608a639023dce3e55e6f90245fb
Rémi Bernon <rbernon@codeweavers.com>
2021-05-21 10:44:22 +0200

    Add a native Win32 platform implementation.
    Even with the engine procedure callback, Wine has to swap it to another
    thread in order for the application-provided callbacks to be executed in
    the context of a Wine thread, and not the SDL audio created thread.
    
    This will also allow Wine get rid of the FAudio SDL2 dependency, which
    is desirable, considering we'll ultimately need FAudio built as PE.

--- src/stb.h
+++ src/stb.h
@@ -207,7 +207,7 @@ CREDITS
  * binding various stdlib functions stb.h uses to FAudio's stdlib.
  * -flibit
  */
-#ifndef FAUDIO_UNKNOWN_PLATFORM
+#ifndef FAUDIO_WIN32_PLATFORM
 #ifdef memcpy /* Thanks Apple! */
 #undef memcpy
 #endif
