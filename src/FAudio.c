FNA-XNA/FAudio

commit b5916945b90d7d1df7af3a2745b675ce9506c0b2

Dont assert on WMA buffer and FAUDIO_FORMAT_EXTENSIBLE type
Monkey island 2.

src/FAudio.c
@@ -2453,7 +2453,9 @@ uint32_t FAudioSourceVoice_SubmitSourceBuffer(
 
 	FAudio_assert(voice->type == FAUDIO_VOICE_SOURCE);
 #ifdef HAVE_WMADEC
-	FAudio_assert(	(voice->src.wmadec != NULL && (pBufferWMA != NULL || voice->src.format->wFormatTag == FAUDIO_FORMAT_XMAUDIO2)) ||
+	FAudio_assert(	(voice->src.wmadec != NULL && (pBufferWMA != NULL ||
+					(voice->src.format->wFormatTag == FAUDIO_FORMAT_XMAUDIO2 ||
+					 voice->src.format->wFormatTag == FAUDIO_FORMAT_EXTENSIBLE)) ||
 			(voice->src.wmadec == NULL && (pBufferWMA == NULL && voice->src.format->wFormatTag != FAUDIO_FORMAT_XMAUDIO2))	);
 #endif /* HAVE_WMADEC */
 

commit 652297b9df91d232612ff1e00c832346330f66ab

Fix unbalanced parentheses in FAudio.c
Commit b5916945b90d ("Dont assert on WMA buffer and
FAUDIO_FORMAT_EXTENSIBLE type") introduced unbalanced parentheses,
which breaks builds using HAVE_WMADEC.

Given the change made in the aforementioned commit, this balances
parentheses by adding a third closing parenthesis after the reworked
conditions.

Signed-off-by: Stephen Kitt <steve@sk2.org>

src/FAudio.c
@@ -2455,7 +2455,7 @@ uint32_t FAudioSourceVoice_SubmitSourceBuffer(
 #ifdef HAVE_WMADEC
 	FAudio_assert(	(voice->src.wmadec != NULL && (pBufferWMA != NULL ||
 					(voice->src.format->wFormatTag == FAUDIO_FORMAT_XMAUDIO2 ||
-					 voice->src.format->wFormatTag == FAUDIO_FORMAT_EXTENSIBLE)) ||
+					 voice->src.format->wFormatTag == FAUDIO_FORMAT_EXTENSIBLE))) ||
 			(voice->src.wmadec == NULL && (pBufferWMA == NULL && voice->src.format->wFormatTag != FAUDIO_FORMAT_XMAUDIO2))	);
 #endif /* HAVE_WMADEC */
 