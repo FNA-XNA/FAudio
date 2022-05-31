e57b60443fc604ae631bbaf447ffc49f2db940de
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2022-06-01 09:54:20 +1000

    Send NOTIFY_CUESTOP when Stop is called

--- src/FACT.c
+++ src/FACT.c
@@ -2735,6 +2735,8 @@ uint32_t FACTCue_SetMatrixCoefficients(
 		}
 	}
 
+	FACT_INTERNAL_SendCueNotification(pCue, NOTIFY_CUESTOP, FACTNOTIFICATIONTYPE_CUESTOP);
+
 	FAudio_PlatformUnlockMutex(pCue->parentBank->parentEngine->apiLock);
 	return 0;
 }
