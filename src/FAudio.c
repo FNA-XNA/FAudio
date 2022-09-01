089a83d0575c2a5efb1c0f310f298c676ebd1e23
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2022-09-01 10:05:30 +1000

    Only get device details if required.

--- src/FAudio.c
+++ src/FAudio.c
@@ -685,9 +685,13 @@ uint32_t FAudio_CreateMasteringVoice(
 	/* For now we only support one allocated master voice at a time */
 	FAudio_assert(audio->master == NULL);
 
-	if (FAudio_GetDeviceDetails(audio, DeviceIndex, &details) != 0)
+	if (	InputChannels != FAUDIO_DEFAULT_CHANNELS ||
+		InputSampleRate != FAUDIO_DEFAULT_SAMPLERATE	)
 	{
-		return FAUDIO_E_INVALID_CALL;
+		if (FAudio_GetDeviceDetails(audio, DeviceIndex, &details) != 0)
+		{
+			return FAUDIO_E_INVALID_CALL;
+		}
 	}
 
 	*ppMasteringVoice = (FAudioMasteringVoice*) audio->pMalloc(sizeof(FAudioVoice));
