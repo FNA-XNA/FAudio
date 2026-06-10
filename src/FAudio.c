FNA-XNA/FAudio

commit 5df7ccfb8a12746f9abf6c83b1e2fb1f3f1b8f11

faudio: Fixup invalid WMA bitrate.
Fixes menu music in Saints Row 2.

src/FAudio.c
@@ -269,6 +269,68 @@ void FAudio_UnregisterForCallbacks(
 	LOG_API_EXIT(audio)
 }
 
+/* WMA decoding is dependent on the bitrate. Only a certain set of bitrates are
+ * actually documented as valid for xWMA.
+ *
+ * The documented bitrates for XACT (aWMAAvgBytesPerSec) as encoded into the
+ * wavebank are often incorrect, at least as late as the August 2008 SDK
+ * (XACT version 3.2). The actual WMA data is encoded at a different bitrate
+ * than the wavebank header apparently states. Similarly, the standalone xWMA
+ * encoder produces xWMA files with an incorrect bitrate in the format chunk.
+ *
+ * While we could fix this in XACT, testing shows that XAudio will do some
+ * degree of fixing up invalid bitrates itself.
+ * The algorithm doesn't seem to be quite the same as here, but this should
+ * at least take care of normalizing all invalid bitrates generated by the
+ * xWMA encoder into the correct valid bitrates that reflect the actual data. */
+static uint32_t fixup_wma_bitrate(FAudio *audio, const FAudioWaveFormatEx *format)
+{
+	static const struct
+	{
+		uint16_t channel_count;
+		uint32_t sample_rate;
+		uint32_t bytes_per_second[5];
+	}
+	rates[] =
+	{
+		{6, 48000, {16000, 24000}},
+		{2, 48000, {6000, 8000, 12000, 20000, 24000}},
+		{6, 44100, {16000, 24000}},
+		{2, 44100, {4000, 6000, 12000, 24000}},
+		{1, 44100, {4000, 6000}},
+		{2, 32000, {4000, 6000}},
+		{1, 32000, {2500}},
+		{2, 22050, {4000}},
+		{1, 22050, {2500}},
+	};
+
+	for (unsigned int i = 0; i < sizeof(rates)/sizeof(*rates); ++i)
+	{
+		if (format->nChannels == rates[i].channel_count
+			&& format->nSamplesPerSec == rates[i].sample_rate)
+		{
+			/* Return the highest valid rate that the input
+			 * sample rate is higher than. If it's lower than the
+			 * lowest valid rate, we return that instead. */
+			uint32_t ret = rates[i].bytes_per_second[0];
+
+			for (unsigned int j = 0; j < 5 && rates[i].bytes_per_second[j]; ++j)
+			{
+				if (format->nAvgBytesPerSec > rates[i].bytes_per_second[j])
+					ret = rates[i].bytes_per_second[j];
+			}
+
+			if (ret != format->nAvgBytesPerSec)
+				LOG_WARNING(audio, "Fixing up invalid sample rate %u to %u.",
+					format->nAvgBytesPerSec, ret);
+			return ret;
+		}
+	}
+
+	LOG_ERROR(audio, "Unexpected channel count %u, sample rate %u.", format->nChannels, format->nSamplesPerSec);
+	return format->nAvgBytesPerSec;
+}
+
 uint32_t FAudio_CreateSourceVoice(
 	FAudio *audio,
 	FAudioSourceVoice **ppSourceVoice,
@@ -477,6 +539,8 @@ uint32_t FAudio_CreateSourceVoice(
 		{
 #ifdef HAVE_WMADEC
 			uint32_t hr;
+
+			fmtex->Format.nAvgBytesPerSec = fixup_wma_bitrate(audio, &fmtex->Format);
 
 			if ((hr = FAudio_WMADEC_init(*ppSourceVoice, fmtex->SubFormat.Data1)))
 			{

commit 81e6a823a5945952febe319647c1c979602aff02

faudio: Don't fixup wma bitrate if it's already valid.
Fixes music in Gridrunner Revolution and other games,
cf. https://bugs.winehq.org/show_bug.cgi?id=59841.

Fixes: 5df7ccfb8a12746f9abf6c83b1e2fb1f3f1b8f11

src/FAudio.c
@@ -337,7 +337,7 @@ static uint32_t fixup_wma_bitrate(FAudio *audio, const FAudioWaveFormatEx *forma
 
 			for (unsigned int j = 0; j < 5 && rates[i].bytes_per_second[j]; ++j)
 			{
-				if (format->nAvgBytesPerSec > rates[i].bytes_per_second[j])
+				if (format->nAvgBytesPerSec >= rates[i].bytes_per_second[j])
 					ret = rates[i].bytes_per_second[j];
 			}
 